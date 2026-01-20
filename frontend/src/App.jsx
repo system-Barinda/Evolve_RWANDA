6f7ffa8611163c3c6028a5146b7a81b4df7d3ad4
system sylvere BARINDA <systembarinda@gmail.com>
2026-01-20 17:47:22 +0200

    header now are complited now to be designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css'
-
+import { createIcons, Menu } from 'lucide';
+createIcons({ icons: { Menu,},});
 function App() {
  
   return(
     <>
-    <header>
-      <div className="leftSide">
-        <h1>evolve</h1>
-       <h4>solving today,shoping tomorrow</h4>
+    <body className="bg-blue-700 text-white">
+      
+   
+    <header className="flex   justify-between gap-15 ">
+      <div className="leftSide h-[100%]   ml-3">
+        <h1 className="text-6xl">evolve</h1>
+       <h4 className="">solving today,shoping tomorrow</h4>
       </div>
-      <div className="rightSide">
-        <div className="divMenu">
-          
+      <div className="rightSide rounded-md  mr-3 mt-5 bg-green-300 text-white h-10 w-10 flex justify-center items-center text-center ">
+        <div className="divMenu ">
+            <i data-lucide="menu" class="w-6 h-6 text-gray-100"></i>
         </div>
       </div>
     </header>
+     </body>
     </>
   )
 }
